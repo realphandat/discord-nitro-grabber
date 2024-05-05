7145bd4379dc33255b5a2a755b2f4d1af64da1a3
realphandat <155445899+realphandat@users.noreply.github.com>
2024-05-05 13:39:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import string
 class color:
 	green = '\033[92m'
 	red = '\033[91m'
-	gray = '\033[90m'
 	reset = '\033[0m'
 
 def start():
-	nitro = "".join(choices(string.ascii_uppercase + string.digits + string.ascii_lowercase, k=16))
+	nitro = "".join(choices(string.ascii_uppercase + string.digits + string.ascii_lowercase, k = 16))
 	res = get(f"https://discordapp.com/api/v6/entitlements/gift-codes/{nitro}?with_application=false&with_subscription_plan=true")
 	if res.status_code == 200:
 		print(f"{color.green}[VALID]{color.reset} - https://discord.gift/{nitro}")
